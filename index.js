b16922b11da11d2205c8dc6ff8f0c6da56064afe
FernandoLuna14 <ferherlun@gmail.com>
2025-11-12 11:25:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//prueba2
+//prueba3
 // === IMPORTAR DEPENDENCIAS ===
 const express = require('express');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
@@ -353,6 +353,7 @@ process.on('uncaughtException', (error) => {
 // === INICIALIZAR BOT ===
 console.log('🚀 Inicializando bot de WhatsApp...');
 client.initialize();
+
 
 
 
